16802323ca974f62716a59ebd9872075b9998c6c
Dmitrijs Bambulaks <dmitrrijs.bambulaks@gmail.com>
2017-12-11 01:31:39 +0100

    ss

--- src/settings.php
+++ src/settings.php
@@ -3,7 +3,7 @@ return [
     'settings' => [
         'displayErrorDetails' => true, // set to false in production
         'addContentLengthHeader' => false, // Allow the web server to send the content-length header
-        "secure" => true,
+        'secure' => true,
 
         // Monolog settings
         'logger' => [
