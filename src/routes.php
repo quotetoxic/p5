03b4fcd6520cb5912d6312709e9a48e4c8a642eb
Dmitrijs Bambulaks <dmitrrijs.bambulaks@gmail.com>
2018-01-17 01:20:33 +0100

    sdad

--- src/routes.php
+++ src/routes.php
@@ -12,3 +12,9 @@ $app->get('/[{name}]', function (Request $request, Response $response, array $ar
     // Render index view
     return $this->renderer->render($response, 'index.phtml', $args);
 });
+
+$app->get('/login', function (Request $request, Response $response, array $args) {
+
+    // Render index view
+    return 'login';
+});
