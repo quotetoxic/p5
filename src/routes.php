4f1d437538c8f44e94e1c0e8df086a9a3010b44c
Dmitrijs Bambulaks <dmitrrijs.bambulaks@gmail.com>
2018-01-17 03:17:54 +0100

    xasxax

--- src/routes.php
+++ src/routes.php
@@ -29,10 +29,11 @@ $app->post('/login', function (Request $request, Response $response, array $args
         return $this->response->withStatus(403)->withHeader('Content-type', 'application/json')->withJson(array('error' => $error));
     } else {
         $user = $result->fetch_all();
+        $exp = time() + (60 * 60 * 24);
         $jwtPayload = [      
             "hash" => $user['hash'],
             "email" => $user['email'],
-            "exp" => (time() + (60 * 60 * 24))
+            "exp" => $exp
               ];
         $user['jwt'] = createJWT($jwtPayload);
         return json_encode($user);
