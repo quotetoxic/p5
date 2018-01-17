3ebecc2d89e93e2b17dae8d545ed1ee9a02b482d
Dmitrijs Bambulaks <dmitrrijs.bambulaks@gmail.com>
2018-01-17 01:26:28 +0100

    nlnljkj

--- src/routes.php
+++ src/routes.php
@@ -5,7 +5,7 @@ use Slim\Http\Response;
 
 // Routes
 
-$app->get('/login/', function (Request $request, Response $response, array $args) {
+$app->get('/login', function (Request $request, Response $response, array $args) {
 
     // Render index view
     return 'login';
