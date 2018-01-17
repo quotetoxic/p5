f0a63c685a22070e0ecf263168a05c8888ef64f3
Dmitrijs Bambulaks <dmitrrijs.bambulaks@gmail.com>
2018-01-17 01:30:44 +0100

    methods

--- src/routes.php
+++ src/routes.php
@@ -5,8 +5,51 @@ use Slim\Http\Response;
 
 // Routes
 
-$app->get('/login', function (Request $request, Response $response, array $args) {
+$app->post('/login', function (Request $request, Response $response, array $args) {
 
     // Render index view
     return 'login';
 });
+
+$app->post('/register', function (Request $request, Response $response, array $args) {
+
+    // Render index view
+    return 'register';
+});
+
+$app->post('/forgotPasswd', function (Request $request, Response $response, array $args) {
+
+    // Render index view
+    return 'forgot';
+});
+
+$app->post('/checkUser', function (Request $request, Response $response, array $args) {
+
+    // Render index view
+    return 'checkUser';
+});
+
+$app->post('/getKey', function (Request $request, Response $response, array $args) {
+
+    // Render index view
+    return 'getKey';
+});
+
+$app->post('/setKey', function (Request $request, Response $response, array $args) {
+
+    // Render index view
+    return 'setKey';
+});
+
+$app->post('/updatePasswd', function (Request $request, Response $response, array $args) {
+
+    // Render index view
+    return 'updatePasswd';
+});
+
+$app->post('/updateInfo', function (Request $request, Response $response, array $args) {
+
+    // Render index view
+    return 'updateInfo';
+});
+
